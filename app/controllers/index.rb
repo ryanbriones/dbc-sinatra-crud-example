09abd6c05609bf167e274b01d7731f1e05a96ec1
Ryan Briones <ryan.briones@brionesandco.com>
2013-10-09 10:21:41 -0500

    add comments to routes relating them to their CRUD equivalents

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,15 @@
+# "INDEX", part of "R" in CRUD
 get '/' do
   @contacts = Contact.all
   erb :index
 end
 
+# "NEW", part of the C in CRUD
 get '/contacts/new' do
   erb :new
 end
 
+# "CREATE", part of the C in CRUD
 post '/contacts' do
   @contact = Contact.create(params[:contact])
   if @contact.valid?
